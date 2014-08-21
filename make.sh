6e100a930b27994fbb7c3c70224c3b6592760072
Natanael Arndt <arndtn@gmail.com>
2014-08-21 14:36:48 +0200

    Update make process

--- make.sh
+++ make.sh
@@ -3,11 +3,17 @@
 # Die XML Eingabedaten werden in dem Ordner Daten erwartet. Die Ausgaben werden in den aktuellen
 # Ordner geschrieben.
 
+files=""
 
 for file in $@
 do
     basename=${file##*/}
-    newfile=${basename%.xml}.rdf
-    echo "Konvertiere ${file} in ${newfile} …"
-    xsltproc tbl-transform.xsl ${file} | xsltproc normlize-space.xsl - > ${newfile}
+    newrdf=${basename%.xml}.rdf
+    newttl=${basename%.xml}.ttl
+    echo "Konvertiere ${file} in ${newrdf} …"
+    xsltproc tbl-transform.xsl ${file} | xsltproc normlize-space.xsl - > ${newrdf}
+    rapper -i 'rdfxml' -o 'turtle' ${newrdf} > ${newttl}
+    files="${files} ${newttl}"
 done
+cat ${files} > all.ttl
+rapper -o 'rdfxml' -i 'turtle' all.ttl > all.rdf
