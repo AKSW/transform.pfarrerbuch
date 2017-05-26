811918daa857627c03405f2284c6c7d5090240a8
Ann-Kathrin Hartmann <annkathrin.hartmann91@gmail.com>
2017-05-26 12:57:24 +0200

    Create parser to separate valid and invalid triples Issue #3

--- TripleChecker/src/test/java/org/aksw/transform/pfarrerbuch/AppTest.java
+++ TripleChecker/src/test/java/org/aksw/transform/pfarrerbuch/AppTest.java
@@ -3,6 +3,15 @@ package org.aksw.transform.pfarrerbuch;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 
 /**
  * Unit test for simple App.
@@ -28,11 +37,29 @@ public class AppTest
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testValidDate() {
+        final String ttlTriple = "<http://pfarrerbuch.aksw.org/kps/person/4495> <http://purl.org/vok/hp/birthDate> \"1615-02-28\"^^<http://www.w3.org/2001/XMLSchema#date> .\n";
+        final Triple triple = getTriple(ttlTriple);
+        final TripleValidator validator = new TripleValidator();
+
+        assertTrue("28th February 1615 is valid", validator.validate(triple));
     }
+
+    public void testInvalidDate() {
+        final String ttlTriple = "<http://pfarrerbuch.aksw.org/kps/person/4495> <http://purl.org/vok/hp/birthDate> \"1615-02-29\"^^<http://www.w3.org/2001/XMLSchema#date> .\n";
+        final Triple triple = getTriple(ttlTriple);
+        final TripleValidator validator = new TripleValidator();
+
+        assertFalse("29th February 1615 not valid", validator.validate(triple));
+    }
+
+    private Triple getTriple(final String ttlTriple) {
+        final Model model = ModelFactory.createDefaultModel();
+        final InputStream reader = new ByteArrayInputStream(ttlTriple.getBytes());
+
+        RDFDataMgr.read(model, reader, Lang.TTL);
+
+        return model.listStatements().toList().get(0).asTriple();
+    }
+
 }
